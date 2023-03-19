ea336b5e748faa8d0de03fa215412e0a4be77656
jpstaub <jpstaub1@gmail.com>
2023-03-19 15:06:59 -0400

    Update gbxmlMerge_streamlit.py

--- gbxmlMerge_streamlit.py
+++ gbxmlMerge_streamlit.py
@@ -180,8 +180,8 @@ for v in vin:
     if True in v:
         sfoc.append(v.index(True))
     else:
-        # sfoc.append(False) #False is equivalent to 0 index in 'sf' check below        
-        sfoc.append('False')       
+        sfoc.append(False) #False is equivalent to 0 index in 'sf' check below        
+        # sfoc.append('False')       
            
 
 # insert: gbxml_B opening into gbxml_C surface object if opening within variable 'dist' parameter
@@ -190,8 +190,8 @@ j = 0
 infos = []
 errors = []
 for sf in sfoc:
-    # if sf==False: #False is equivalent to 0 index    
-    if sf=='False':
+    if sf==False: #False is equivalent to 0 index    
+    # if sf=='False':
         infos.append('Opening without matching surface: ' + ops[i].Name.text + '.')
         j+=1
         i+=1
@@ -215,7 +215,10 @@ for sf in sfoc:
 with st.expander('Status'):
     st.info('Copied ' + str(i-j) + ' of ' + str(len(ops)) + ' openings.')
     for i in infos:
-        st.write(i)          
+        if 'Copied' in i:
+            st.write(i)
+        else:
+            st.error(i)
 
 # write errors to an expander container
 with st.expander('Exceptions'):
